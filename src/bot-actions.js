968957742bfad9bbebb2c48133fc7f3286164b6b
Kostya Lukianjanenko <kirov1607@gmail.com>
2023-11-21 22:49:19 +0200

    Added the send goodnight text in every 23 hours 55 minutes

--- src/bot-actions.js
+++ src/bot-actions.js
@@ -1,11 +1,34 @@
 const bot = require('./bot');
-const {getValidMessage, getCurrentTimeInfo} = require('./schedule');
+const { getValidMessage, getCurrentTimeInfo } = require('./schedule');
+
+let timer = null;
 
 function botActions() {
   bot.onText(/^с?сылка пара$/gi, async (m) => {
     const result = await getValidMessage();
     bot.sendMessage(m.chat.id, result);
   });
+  bot.onText(/^старт желание$/gi, async (m) => {
+    timer = sayGoodnight();
+  });
+  bot.onText(/^стоп желание$/gi, async (m) => {
+    clearInterval(timer);
+    timer = null;
+  });
+  bot.onText(/^таймер$/gi, async (m) => {
+    bot.sendMessage(m.chat.id, JSON.stringify(timer));
+  });
+}
+
+function sayGoodnight() {
+  const updatesTimeMs = 1000 * 60;
+  timer = setInterval(() => {
+    const { getHours, getMinutes } = getCurrentTimeInfo();
+    if (getHours === 23 && getMinutes === 55) {
+      const message = 'Доброй ночи, господа!';
+      bot.sendMessage(-4078181398, message);
+    }
+  }, updatesTimeMs);
 }
 
 module.exports = botActions;
